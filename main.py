8e6095d265617df68c5f22ad90e64879017c723f
An <vananle1993@gmail.com>
2023-09-06 14:33:03 +0900

    add FL _training

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ if __name__ == '__main__':
     # exit()
 
     # FL training
-    fl_train = FL_Learning(n_clients=5, n_train_round=10)
+    fl_train = FL_Learning(n_clients=1, n_train_round=10)
     fl_train.init_clients()
     fl_train.train_clients()
 
