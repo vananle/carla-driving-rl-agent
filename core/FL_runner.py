bc347dedef1b922e66f2d0c8314fb87b274f45d5
An <vananle1993@gmail.com>
2023-09-07 10:55:23 +0900

    FL traning

--- core/FL_runner.py
+++ core/FL_runner.py
@@ -287,6 +287,7 @@ class FL_Learning:
 
             global_weights = self.calculate_global_weights(client_weights,
                                                            n_trained_clients=len(random_client_idx))
+            print(global_weights.keys)
             for client_id in client_idx:
                 client = self.clients[client_id]
                 client.update_weights(global_weights)
